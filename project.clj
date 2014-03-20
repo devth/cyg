367ab1afc222deea52e27221d578234391c256c0
Trevor Hartman <trevorhartman@gmail.com>
2014-03-20 06:50:00 -0600

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cyg "0.1.0"
+(defproject cyg "0.1.1-SNAPSHOT"
   :description "Minimalist config for Clojure"
   :url "https://github.com/devth/cyg"
   :lein-release {:deploy-via :clojars}

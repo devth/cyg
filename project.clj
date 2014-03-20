d78a15f280e34c29648de8070b1046cc0aabe143
Trevor Hartman <trevorhartman@gmail.com>
2014-03-20 06:49:50 -0600

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cyg "0.1.0-SNAPSHOT"
+(defproject cyg "0.1.0"
   :description "Minimalist config for Clojure"
   :url "https://github.com/devth/cyg"
   :lein-release {:deploy-via :clojars}
